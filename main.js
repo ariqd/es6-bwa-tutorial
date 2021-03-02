60563753180be8a56f7d3e019734b4982cb911be
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-03-02 16:09:58 +0700

    skills array

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("hello world")
+// Ambil element
+const skills = document.getElementById("skills");
+
+// ES 6
+const mySkills = ["UX Design", "Web Development", "Video Editor"];
+// console.log(mySkills[0]);
+// skills.innerHTML = mySkills[0];
+
+mySkills.push("Dev Ops");
+
+let parent = "<ul>";
+
+mySkills.forEach((skill) => {
+  console.log(skill);
+  parent += `<li>${skill}</li>`;
+});
+
+parent += "</ul>";
+
+skills.innerHTML = parent;
