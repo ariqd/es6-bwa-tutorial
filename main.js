901eeb8c2e0f592a232d2fd42145001a168e7761
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-03-02 16:33:07 +0700

    init student class

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 // Ambil element
 const data = document.getElementById("data");
+
+// ES6
+// Modul untuk member kelas
+class Student {
+  constructor(username, password, id_kelas) {
+    this.username = username;
+    this.password = password;
+    this.id_kelas = id_kelas;
+  }
+
+  join() {
+    console.log(
+      this.username + " telah bergabung pada kelas " + this.id_kelas
+    );
+  }
+}
+
+let addStudent = new Student("ariqd", "123123", 33);
+
+addStudent.join();
