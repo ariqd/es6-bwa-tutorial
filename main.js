c4960f041e95b769581c83c1a4688e7a5c1f17ca
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-03-02 17:02:35 +0700

    better output

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 // ES5: var
-var myCars = ["BMW", "Tesla", "Toyota"];
+var myCars1 = ["BMW", "Tesla", "Toyota"];
 
-for (var i = 0; i < myCars.length; i++) {
-   console.log(myCars[i]);
- }
+for (var i = 0; i < myCars1.length; i++) {
+  console.log("myCars1: " + myCars1[i]);
+}
 
- console.log(i); // 3
+console.log(i); // 3
 
 // ES 6: let
 // Mencegah variabel diinisalisasi diluar scope
-const myCars = ["BMW", "Tesla", "Toyota"];
+const myCars2 = ["BMW", "Tesla", "Toyota"];
 
-for (let i = 0; i < myCars.length; i++) {
-  console.log(myCars[i]);
+for (let x = 0; x < myCars2.length; x++) {
+  console.log("myCars2: " + myCars2[x]);
 }
 
-console.log(i); // Error
+console.log(x); // Error
