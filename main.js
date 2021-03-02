2cd9d80f2d24a588b8e77a76c647cac0e9797caa
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-03-02 16:27:30 +0700

    init section 7

--- main.js
+++ main.js
@@ -1,22 +1,2 @@
 // Ambil element
-const skills = document.getElementById("skills");
-const primarySkill = document.getElementById("primarySkill");
-
-// Inisialisasi Array
-const mySkills = ["UX Design", "Web Development", "Video Editor"];
-
-// Map
-const printSkills = mySkills.map((skill) => {
-  return skill;
-});
-
-skills.innerHTML = printSkills;
-
-// Filter
-const myPrimarySkill = mySkills.filter((skill) => {
-  return skill === "UX Design"; // -> return yang sama dengan UX Design
-  //   return skill !== "Video Editor"; // -> return selain video editor
-});
-
-console.log(myPrimarySkill);
-primarySkill.innerHTML = myPrimarySkill;
+const data = document.getElementById("data");
